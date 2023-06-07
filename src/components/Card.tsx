b4bc02a887d74e9a05c8a67c278e51ccab61a255
brandon.stryker@goftx.com <brandon.stryker@goftx.com>
2023-06-07 12:44:53 -0400

    stubbed out color theme switch

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,13 @@
 import {FC} from "react";
+import {ColorTheme} from "../App";
 
-const Card: FC<{linkText: string}> = ({linkText}) => (
-  <div className="w-64 p-16 rounded-xl border-solid border-2 border-fuchsia-100">
-    <p className="text-xl text-white">{linkText}</p>
-  </div>
-);
+const Card: FC<{colors: ColorTheme; linkText: string;}> = ({colors, linkText}) => {
+
+    return (
+        <div style={{borderColor: colors.accent}} className={`w-64 p-16 rounded-xl border-solid border-2 text-center`}>
+            <p style={{color: colors.foreground}} className={`text-xl`}>{linkText}</p>
+        </div>
+    );
+}
 
 export default Card;
