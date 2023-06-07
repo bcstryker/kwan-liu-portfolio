96837fc56ad6dc458d04417a7cdd6bc732c26550
Brandon Stryker <bcstryker@gmail.com>
2023-06-07 13:09:38 -0400

    fixed dark/light mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,38 @@ import ThemeSwitch from "./components/ThemeSwitch";
 function App() {
   const [darkTheme, setDarkTheme] = useState(true);
   const [colors, setColors] = useState<ColorTheme>({
-    foreground: "black",
-    background: "white",
+    foreground: "white",
+    background: "black",
     accent: "fuchsia-100",
-  })
+  });
   const switchTheme = useCallback(() => {
     setColors({
-      foreground: darkTheme ? "white" : "black",
       background: darkTheme ? "black" : "white",
+      foreground: darkTheme ? "white" : "black",
       accent: darkTheme ? "fuchsia" : "cyan",
     });
   }, [darkTheme]);
   useEffect(() => {
-    switchTheme()
-  }, [darkTheme])
+    switchTheme();
+  }, [switchTheme]);
 
   return (
     <div className="App">
       <head>
         <title>Kwan Liu</title>
       </head>
-      <body className={`bg-${colors.background}`}>
+      <body style={{backgroundColor: colors.background}}>
         <ThemeSwitch colors={colors} setDarkTheme={setDarkTheme} />
         <div className="h-screen">
           <div className="flex flex-col pt-[35vh] justify-center h-1/2">
-            <h1 style={{color: colors.foreground}} className={`text-4xl text-center`}>Kwan Liu</h1>
-            <h1 style={{color: colors.foreground}} className={`text-2xl text-center`}>Product Designer</h1>
+            <h1 style={{color: colors.foreground}} className={`text-4xl text-center`}>
+              Kwan Liu
+            </h1>
+            <h1 style={{color: colors.foreground}} className={`text-2xl text-center`}>
+              Product Designer
+            </h1>
           </div>
-          <div className={`pt-24 py-24 flex justify-center h-1/2 bg-${colors.background}`}>
+          <div className={`pt-24 py-24 flex justify-center h-1/2`}>
             <table className="border-collapse border-spacing-16">
               <tbody>
                 <tr>
